3819b551f8c0d4bad3cd88a4419b715edeb86b4f
Alexis.Benavente <lxsbenavente@gmail.com>
2012-04-17 22:44:02 -0500

    primera subida

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pelotero::Application.config.secret_token = 'd078d4b41cdc836e83573a423cb3dc92f033295d2d39af0cc973a2c8512ae987301157a367dd56f28f9c91c2289ae8819026f1e9e26a4c8c88be139154cb12bd'
---
+++ db/migrate/20120418033847_create_locals.rb
@@ -0,0 +1,12 @@
+class CreateLocals < ActiveRecord::Migration
+  def change
+    create_table :locals do |t|
+      t.string :name
+      t.string :address
+      t.references :owner
+
+      t.timestamps
+    end
+    add_index :locals, :owner_id
+  end
+end
